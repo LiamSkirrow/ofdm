8a59bb95af6d002ba0c5bad1497093e8f14a4bd1
Liam Skirrow <liam.skirrow@morsemicro.com>
2023-11-04 20:57:40 +0000

    Added an example function in the time domain to try as a proof of concept

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ if __name__ == "__main__":
     fs = 1/0.000000050  # 20Msps -> 50ns sampling period
     T = N/(fs)          # 64 * 1/20MHz -> 64 * 50ns -> 3.2us
     freqBins = n/T      # 64 bins of 312.5kHz spacing (20MHz/64)
+
+    f1 = 312500
+    sine0  = 0.5*np.sin(2*np.pi*f1*t)
+    f2 = 625000
+    sine0 += 2*np.sin(2*np.pi*f2*t)
+    f3 = 937500
+    sine0 += 3*np.sin(2*np.pi*f3*t)
     
     mapped_complex_values = np.array([1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j,
                                       1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j, 1+1j,
@@ -32,16 +39,24 @@ if __name__ == "__main__":
 
     fig1, ax1 = plt.subplots()
     fig2, ax2 = plt.subplots()
+    fig3, ax3 = plt.subplots()
+    fig4, ax4 = plt.subplots()
     ax1.plot(freqBins, mapped_ifft.real, label='real')
     ax1.plot(freqBins, mapped_ifft.imag, '--', label='imaginary')
 
     calc_fft = fft(mapped_ifft)
     ax2.plot(t, calc_fft)
 
+    ax3.plot(t, sine0)
+    ax4.plot(freqBins, np.abs(fft(sine0)), label='real')
+    # ax4.plot(freqBins, fft(sine0).imag, '--', label='imaginary')
+
     ax1.grid()
     ax1.legend()
     ax2.grid()
     ax2.legend()
+    ax3.grid()
+    ax3.legend()
 
     print(freqBins)
 
