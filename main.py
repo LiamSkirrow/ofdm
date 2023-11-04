139d40ad170e972668b49e8435d66a82b2b886d7
Liam Skirrow <liam.skirrow@morsemicro.com>
2023-11-04 11:01:46 +0000

    Added hacky FFT calculation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.fft import fft, ifft
 # import scipy as sc
 
 if __name__ == "__main__":
@@ -9,8 +10,18 @@ if __name__ == "__main__":
     f = 10
     sine = np.sin(2*np.pi*f*t)
 
-    fig, ax = plt.subplots()
-    ax.plot(t,sine)
-    ax.grid()
+    Xsine = fft(sine)
+    N = len(Xsine)
+    n = np.arange(N)
+    T = N/(1/0.001)
+    freq = n/T
+
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    ax1.plot(t,sine)
+    ax2.plot(freq,np.abs(Xsine))
+
+    ax1.grid()
+    ax2.grid()
 
     plt.show()
